823a67f1f4bae6cbd1bc0a60212655451414dbd0
Jason Woodward <woodwardj@jaos.org>
2004-05-25 02:49:28 +0000

    initialized global_config->re_install

--- src/configuration.c
+++ src/configuration.c
@@ -45,6 +45,7 @@ rc_config *read_rc_config(const char *file_name){
 	global_config->print_uris = 0;
 	global_config->dl_stats = 0;
 	global_config->no_prompt = 0;
+	global_config->re_install = 0;
 	/* global_config->exclude_list->count = 0; */ /* initialized later */
 
 	rc = open_file(file_name,"r");
