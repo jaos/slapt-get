fae21f7540cc23a20e5211de406b95e0f5d592da
Jason Woodward <woodwardj@jaos.org>
2004-08-22 17:14:21 +0000

    use libcurls CURLOPT_FAILONERROR to fail on http codes > 300

--- src/curl.c
+++ src/curl.c
@@ -33,7 +33,8 @@ int download_data(FILE *fh,const char *url,size_t bytes,int use_curl_dl_stats){
 	curl_easy_setopt(ch, CURLOPT_WRITEDATA, fh);
 	curl_easy_setopt(ch, CURLOPT_NOPROGRESS, 0);
 	curl_easy_setopt(ch, CURLOPT_USERAGENT, PROGRAM_NAME );
-	curl_easy_setopt(ch, CURLOPT_FTP_USE_EPSV , 0);
+	curl_easy_setopt(ch, CURLOPT_FTP_USE_EPSV, 0);
+	curl_easy_setopt(ch, CURLOPT_FAILONERROR, 1);
 
 	if( use_curl_dl_stats != 1 ){
 		curl_easy_setopt(ch, CURLOPT_PROGRESSFUNCTION, progress_callback );
@@ -104,6 +105,7 @@ char *head_request(const char *url){
 	curl_easy_setopt(ch, CURLOPT_HEADER, 1);
 	curl_easy_setopt(ch, CURLOPT_NOBODY, 1);
 	curl_easy_setopt(ch, CURLOPT_FTP_USE_EPSV , 0);
+	curl_easy_setopt(ch, CURLOPT_FAILONERROR, 1);
 
 	if( (response = curl_easy_perform(ch)) != 0 ){
 		free(head_t.data);
