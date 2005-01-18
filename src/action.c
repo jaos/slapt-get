7ef9e49f07676226af72501d96364c7aaa07057d
Jason Woodward <woodwardj@jaos.org>
2005-01-18 02:31:11 +0000

    removed duplicate exclude check in upgrade before we check if the package

--- src/action.c
+++ src/action.c
@@ -522,11 +522,6 @@ void pkg_action_upgrade_all(const rc_config *global_config){
 		if( update_pkg != NULL ){
 			int cmp_r = 0;
 
-			if( is_excluded(global_config,installed_pkgs->pkgs[i]) == 1 ){
-				add_exclude_to_transaction(&tran,update_pkg);
-				continue;
-			}
-
 			/* if the update has a newer version, attempt to upgrade */
 			cmp_r = cmp_pkg_versions(installed_pkgs->pkgs[i]->version,update_pkg->version);
 			if(
