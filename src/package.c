0ae6956dd3edf73b45b6fa115ce8d8bd9b7b3662
Jason Woodward <woodwardj@jaos.org>
2008-03-09 04:06:09 +0000

    updating dates to include 2008

--- src/package.c
+++ src/package.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2007 Jason Woodward <woodwardj at jaos dot org>
+ * Copyright (C) 2003-2008 Jason Woodward <woodwardj at jaos dot org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
